37b1080ef346a7a532b74331f480d7af7e574a21
muiga <muigask@gmail.com>
2026-05-10 15:27:05 +0300

    add flatpak 3

--- modules/nixos/features/flatpak.nix
+++ modules/nixos/features/flatpak.nix
@@ -20,22 +20,7 @@
           appId = "com.stremio.Stremio";
           origin = "flathub";
         }
-        {
-          appId = "com.markopejic.downloader";
-          origin = "flathub";
-        }
-        {
-          appId = "org.gtk.Gtk3theme.adw-gtk3";
-          origin = "flathub";
-        }
-        {
-          appId = "org.gtk.Gtk3theme.adw-gtk3-dark";
-          origin = "flathub";
-        }
-        {
-          appId = "io.github.cosmic_utils.cosmic-ext-applet-clipboard-manager";
-          origin = "cosmic";
-        }
+
       ];
 
       services.flatpak.uninstallUnmanaged = true;
